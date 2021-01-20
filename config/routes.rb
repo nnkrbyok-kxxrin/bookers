7acac567e0840172113a23714329d926dab67569
nnkrbyok-kxxrin <kxxxx.mxxxkx@gmail.com>
2021-01-20 10:24:16 +0000

    一時保存

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :edit]
   resources :users, only: [:index, :show, :edit, :update]
 end
